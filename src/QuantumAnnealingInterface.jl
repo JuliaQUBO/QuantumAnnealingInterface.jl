445de8f2198e67d890a7ec7fa55d54f6af9596aa
pedromxavier <pedromxavier@poli.ufrj.br>
2022-08-31 14:29:24 -0300

    Adapt to `Anneal >= 0.3.2`

--- src/QuantumAnnealingInterface.jl
+++ src/QuantumAnnealingInterface.jl
@@ -1,31 +1,96 @@
 module QuantumAnnealingInterface
 
-using Anneal
-using QuantumAnnealing
+import Anneal
+
 using MathOptInterface
 const MOI = MathOptInterface
-const VI = MOI.VariableIndex
-const ROA = MOI.RawOptimizerAttribute
 
-struct Optimizer{T} <: Anneal.AbstractSampler{T}
-    x::Dict{VI, Int}
-    y::Dict{Int, VI}
-    s::T
-    Q::Dict{Tuple{Int, Int}, T}
-    c::T
+using LinearAlgebra
+using QuantumAnnealing
 
-    attrs::Dict{String, Any}
+Anneal.@anew Optimizer begin
+    name = "Simulated Quantum Annealer"
+    sense = :min
+    domain = :spin
+    version = v"0.2.0"
+    attributes = begin
+        "num_reads"::Integer = 1_000
+        "annealing_time"::Float64 = 1.0
+        "annealing_schedule"::QuantumAnnealing.AnnealingSchedule = QuantumAnnealing.AS_LINEAR
+        # "steps"::Integer = 0
+        # "order"::Integer = 4
+        # "mean_tol"::Float64 = 1e-6
+        # "max_tol"::Float64=1e-4
+        # "iteration_limit"::Integer = 100
+        # "state_steps"::Union{Integer,Nothing}=nothing
+        # kwargs...
+    end
 end
 
-function Anneal.sample(annealer::Optimizer)
-    _, h, J, c = Anneal.ising_normal_form(annealer)
-    isingmodel = merge(h, J)
+function sample_state(P::Vector{Float64}, n::Integer)
+    @assert length(P) == (m = 2^n)
 
-    @show QuantumAnnealing.simulate(
-        isingmodel,
-        MOI.get(annealer, ROA("annealing_time")),
-        MOI.get(annealer, ROA("annealing_schedule")),
+    # ~*~ Sample p ~ [0, 1] ~*~ #
+    p = rand()
+
+    # ~*~ Run Binary Search ~*~ #
+    i = first(searchsorted(P, p))
+
+    return 2 .* digits(Int, i - 1; base=2, pad=n) .- 1
+end
+
+function Anneal.sample(annealer::Optimizer{T}) where {T}
+    # ~*~ Retrieve Model ~*~ #
+    h, J = Anneal.ising(Dict, T, annealer)
+
+    ising_model = merge(
+        Dict((i,) => w for (i, w) in h),
+        J,
     )
+
+    # ~*~ Retrieve Attributes ~*~ #
+    n = MOI.get(annealer, MOI.NumberOfVariables())
+
+    num_reads          = MOI.get(annealer, MOI.RawOptimizerAttribute("num_reads"))
+    annealing_time     = MOI.get(annealer, MOI.RawOptimizerAttribute("annealing_time"))
+    annealing_schedule = MOI.get(annealer, MOI.RawOptimizerAttribute("annealing_schedule"))
+    silent             = MOI.get(annealer, MOI.Silent())
+
+    # ~*~ Timing Information ~*~#
+    time_data = Dict{String, Any}()
+
+    # ~*~ Run simulation ~*~ #
+    ρ = let results = @timed QuantumAnnealing.simulate(
+            ising_model,
+            annealing_time,
+            annealing_schedule;
+            silence=silent
+        )
+        time_data["simulation"] = results.time
+
+        results.value
+    end
+
+    # ~*~ Measure probabilities ~*~ #
+    p = let results = @timed cumsum(real.(diag(ρ)))
+        time_data["measurement"] = results.time
+
+        results.value
+    end
+
+    # ~*~ Sample states ~*~ #
+    samples = let results = @timed Vector{Int}[sample_state(p, n) for _ = 1:num_reads]
+        time_data["sampling"] = results.time
+
+        results.value
+    end
+
+    metadata = Dict{String,Any}(
+        "time"   => time_data,
+        "origin" => "Quantum Annealing Simulation"
+    )
+
+    return Anneal.SampleSet{Int,T}(annealer, samples, metadata)
 end
 
 end # module
---
+++ test/runtests.jl
@@ -0,0 +1,9 @@
+using Test
+using Anneal
+using QuantumAnnealingInterface
+
+function main()
+    Anneal.test(QuantumAnnealingInterface.Optimizer)
+end
+
+main() # Here we go!
